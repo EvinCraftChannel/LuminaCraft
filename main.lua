a6323ab5f3af89c16170b1ae50e1c1203f65b36d
EvinCraftChannel <sonytracx86@gmail.com>
2026-02-08 20:26:12 +0700

    Refactor server logic to include entity management
    Added EntityManager and EntityPhysics for entity updates.

--- main.lua
+++ main.lua
@@ -2,14 +2,29 @@ local uv = require('uv')
 local ServerProps = require('./src/utils/properties')
 local Network = require('./src/network/packet_handler')
 local World = require('./src/world/leveldb_reader')
-local CustomItemManager = require('./src/item/custom_item_manager')
 local PluginManager = require('./src/plugin/manager')
 local PlayerManager = require('./src/player/manager')
+local EntityManager = require('./src/entity/manager')
+local EntityPhysics = require('./src/entity/physics')
+
 local config = ServerProps.load("server.properties")
+
+_G.Server = {
+    broadcast = function(msg)
+        print("[BROADCAST] " .. msg)
+    end
+}
+
 PluginManager.loadAll("./plugins")
+
 local server = uv.new_udp()
-World.init(4)
-server:bind("0.0.0.0", config.port or 19132)
+local port = tonumber(config["server-port"]) or 19132
+
+World.init(2)
+
+server:bind("0.0.0.0", port)
+print("§a[Lumina] Server " .. config["server-name"] .. " running on port " .. port)
+
 local tick_timer = uv.new_timer()
 tick_timer:start(0, 50, function()
     for _, player in pairs(PlayerManager.getAll()) do
@@ -17,14 +32,16 @@ tick_timer:start(0, 50, function()
     end
     
     for _, entity in pairs(EntityManager.getAll()) do
-        EntityPhysics.update(entity)
+        EntityPhysics.update(entity, World)
     end
 end)
 
 server:recv_start(function(err, data, addr)
+    if err then
+        print("Network Error: " .. tostring(err))
+        return
+    end
     if data then
         Network.handle(data, addr, server, config)
     end
 end)
-
-print("§a[Lumina] Server " .. config["server-name"] .. " running on port " .. config.port)
